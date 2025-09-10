a3b499f9e581d74fe5b1453f43aaf85ec76ff9bf
mariele2105 <marielefernandes.mf.mf@gmail.com>
2025-09-09 21:47:32 -0300

    Adicionando aula05

--- aulas/aula05/index.js
+++ aulas/aula05/index.js
@@ -1,8 +1,21 @@
 // importa o framework 
 const express = require("express");
 
+//importa middleware de terceiros
+const cors = require('cors');
+
+// importa middleware de rota
+const router = require('./routerTarefa');
+
 // criar uma instancia da aplicação
 const app = express();
+
+//middleware embutido ou integrado
+app.use(express.json());
+app.use(express.urlencoded({ extended: false})); 
+
+//middleware de terceiros
+app.use(cors());
 
 //middleware de aplicaçao
 app.use(function(req, res, next) {
@@ -10,23 +23,7 @@ app.use(function(req, res, next) {
   next();
 })
 
-//middlare de rota
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send("Chegou aqui");
-});
-
-router.post('/', (req, res) => {
-    res.status(201).send("Inserido com sucesso");
-})
-
-router.get("/:id", (req, res) => {
-    const { id } = req.params;
-    if (id == 1) return res.send("Achei");
-    throw Error("Não achei");
-})
-
+//middleware de rota 
 app.use('/tarefas', router);
 
 //middleware de erro
@@ -39,4 +36,3 @@ app.use((err, req, res, next) => {
 app.listen(3000, ()=>{
     console.log("APP está ON!");
 });
-
