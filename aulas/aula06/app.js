a4a9bd7149e28f80b6df3cedeeba2b419f3847d5
mariele2105 <marielefernandes.mf.mf@gmail.com>
2025-09-18 21:46:59 -0300

    Adicionando aula 06

--- aulas/aula06/app.js
+++ aulas/aula06/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const tarefas = [];
 
 const indexRouter = require('./routes/index');
+const tarefaRouter = require("./routes/tarefas")
 
 
 const app = express();
@@ -16,26 +16,8 @@ app.use(cookieParser());
 
 
 app.use('/', indexRouter);
+app.use("/tarefas", tarefaRouter);
 
-app.get('/tarefas', (req, res) => {
-    res.json(tarefas);
-})
 
-app.post('/tarefas', (req, res) => {
-    const novaTarefa = { 
-        ...req.body, id: tarefas.length + 1
-    };
-    tarefas.push(novaTarefa);
-    res.status(201).json(novaTarefa);
-});
-
-app.get("/tarefas/:id", (req, res) => {
-    const { id } = req.params; 
-    const tarefaEncontrada = tarefas.find((item) => 
-        item.id === parseInt(id));
-    if (tarefaEncontrada)
-      return res.json(tarefaEncontrada);
-    res.status(404).json({msg: "Tarefa não encontrada"});
-    });
 
 module.exports = app; 
